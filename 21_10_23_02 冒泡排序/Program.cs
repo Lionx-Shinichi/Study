d544b28c4994648ecab2d55cb6f00b297e7b33f0
Lionx Shinichi <Lionx.shinichi@Gmail.com>
2021-10-23 18:45:19 +0800

    增加内容

--- 21_10_23_02 冒泡排序/Program.cs
+++ 21_10_23_02 冒泡排序/Program.cs
@@ -10,6 +10,25 @@ namespace _21_10_23_02_冒泡排序
     {
         static void Main(string[] args)
         {
+            int[] nums = { 1, 7, 5, 3, 9, 8, 6, 4, 2, 0, 5, 2, 1, 5, 5, 1, 2, 3, 5, 6, 3, 1 };
+            //将以上数组升序排列
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp = nums[j + 1];
+                        nums[j + 1] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.Write(nums[j]);
+            }
+            Console.ReadKey();
         }
     }
 }
